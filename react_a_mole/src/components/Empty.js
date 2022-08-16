683a86d902bcde4d5b758a14f3b9d8bdae3c4378
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-08-16 04:24:17 -0400

    Added content

---
+++ react_a_mole/src/components/Empty.js
@@ -0,0 +1,14 @@
+import moleHill from '../embedded/molehill.png';
+
+export default function Empty (props) {
+    useEffect(() => {
+        const showMoleAfter = Math.floor(Math.random() * 5000 + 500);
+        const timer = setTimeout(() => {
+         props.setIsShowing(true);   
+        }, showMoleAfter);
+        return () => {
+            clearTimeout(timer);
+        }
+    })
+    return <img alt='empty mole hill' src={moleHill}/>
+}
